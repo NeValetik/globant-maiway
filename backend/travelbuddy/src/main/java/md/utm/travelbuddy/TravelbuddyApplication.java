5d5e911e1dfe15d864e8d9226fd2c3a476c30b3f
Nicolae M. <nicolaemargamd@gmail.com>
2024-09-09 16:00:00 +0300

    Made an offerdata class

--- backend/travelbuddy/src/main/java/md/utm/travelbuddy/TravelbuddyApplication.java
+++ backend/travelbuddy/src/main/java/md/utm/travelbuddy/TravelbuddyApplication.java
@@ -1,15 +1,19 @@
 package md.utm.travelbuddy;
 
+import md.utm.travelbuddy.tables.OfferData;  // Correct import statement
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TravelbuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelbuddyApplication.class, args);
 		System.out.println("Hello Traveler");
+		OfferData OfferNo1 = new OfferData();
+		OfferNo1.setTitle("This Is offer number1");
+		System.out.println(OfferNo1.getTitle());
 	}
 
 }
