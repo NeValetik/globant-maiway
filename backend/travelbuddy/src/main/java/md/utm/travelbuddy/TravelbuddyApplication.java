71a132f3d145d9eaf0b252b2ffcaa467494f174c
Nicolae M. <nicolaemargamd@gmail.com>
2024-09-10 14:59:21 +0300

    merged

--- backend/travelbuddy/src/main/java/md/utm/travelbuddy/TravelbuddyApplication.java
+++ backend/travelbuddy/src/main/java/md/utm/travelbuddy/TravelbuddyApplication.java
@@ -1,7 +1,5 @@
 package md.utm.travelbuddy;
 
-import md.utm.travelbuddy.tables.OfferData;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,9 +9,6 @@ public class TravelbuddyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TravelbuddyApplication.class, args);
 		System.out.println("Hello Traveler");
-		OfferData OfferNo1 = new OfferData();
-		OfferNo1.setTitle("This Is offer number1");
-		System.out.println(OfferNo1.getTitle());
 	}
 
 }
