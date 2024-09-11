635800eb92695631d3aa51930d01d9bd6f24c2be
NeValetik <snowchug@gmail.com>
2024-09-11 22:35:38 +0300

    Now offers change by the page number

--- frontend/my-project/src/components/Offers.jsx
+++ frontend/my-project/src/components/Offers.jsx
@@ -3,8 +3,9 @@ import Card from  "./Card"
 
 const Offers = () => {
   const [offers, setOffers] = useState([])//here we need to load from database offers I'll be putting some constant data
+  const [page, setPage] = useState(1)
   useEffect(() => {
-    fetch('http://localhost:6969/api/offer/page/1')
+    fetch(`http://localhost:6969/api/offer/page/${page}`)
       .then((res) => {
         return res.json();
       })
