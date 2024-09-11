932d6816dede9afe63a784a4f4415d71b2308dcb
Nicolae M. <nicolaemargamd@gmail.com>
2024-09-11 10:50:16 +0300

    Fixed offer object bug

--- backend/travelbuddy/src/main/java/md/utm/travelbuddy/TravelbuddyApplication.java
+++ backend/travelbuddy/src/main/java/md/utm/travelbuddy/TravelbuddyApplication.java
@@ -2,8 +2,7 @@ package md.utm.travelbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class TravelbuddyApplication {
 
 	public static void main(String[] args) {
