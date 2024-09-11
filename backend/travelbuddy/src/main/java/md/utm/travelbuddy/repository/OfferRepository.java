943bd917f88f49bf3724eb1315bd0ded2c28f263
Timur Сravtov <timurcravtov04@gmail.com>
2024-09-11 12:25:54 +0300

    added query for offset limit

--- backend/travelbuddy/src/main/java/md/utm/travelbuddy/repository/OfferRepository.java
+++ backend/travelbuddy/src/main/java/md/utm/travelbuddy/repository/OfferRepository.java
@@ -2,8 +2,14 @@ package md.utm.travelbuddy.repository;
 
 import md.utm.travelbuddy.models.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+    @Query(value = "SELECT * FROM offers o ORDER BY o.id OFFSET ?1 LIMIT ?2", nativeQuery = true)
+    List<Offer> getPageLimitedOffers(int offset, int limit);
 }
