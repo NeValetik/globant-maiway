2f7aead4af920bd151e178f8316b3d5a044b1dc3
NeValetik <snowchug@gmail.com>
2024-09-11 13:40:39 +0300

    [scrum 5, scrum 4] refactor + fix

--- frontend/my-project/src/components/Offers.jsx
+++ frontend/my-project/src/components/Offers.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Card from  "./Card"
 
 const Offers = () => {
   const [offer, setOffer] = useState({"username":"Anton", "location":"Warshaw", "description":"Ne ducem dupa piva", "image":"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQgSvVwAdK_66hOT_ut0rKqjeYbqeD7qfjN31wUqpfiNKTAqhFv"});//here we need to load from database offers I'll be putting some constant data
@@ -11,55 +11,11 @@ const Offers = () => {
   return (
 
 <div className="flex flex-row gap-10 mx-auto justify-center items-center">
-  <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img className="w-full h-40" src={offer.image} alt="Sunset in the mountains" />
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">
-        {offer.location}
-        </div>
-      <p className="text-gray-700 text-base">
-        {offer.description}
-      </p>
-    </div>
-    <div className="px-6 pt-4 pb-2">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-    </div>
-  </div>
-
-  <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img className="w-full h-40" src="https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2016/05/20/1331795954995_2/%D1%80%D0%B0%D0%B9%D0%B0%D0%BD-%D0%B3%D0%BE%D1%81%D0%BB%D0%B8%D0%BD%D0%B3.jpg" alt="Sunset in the mountains" />
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-      <p className="text-gray-700 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-      </p>
-    </div>
-    <div className="px-6 pt-4 pb-2">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-    </div>
-  </div>
-
-  <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img className="w-full h-40 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9jEvprovmviLVeuY5cHd5Pu9ZMmuD-oIspg&s" alt="Sunset in the mountains" />
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-      <p className="text-gray-700 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-      </p>
-    </div>
-    <div className="px-6 pt-4 pb-2">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-    </div>
-  </div>
+  <Card offer={offer}/>
+  <Card offer={offer}/>
+  <Card offer={offer}/>
 </div>
-
-  );
+)
 };
 
 export default Offers;
