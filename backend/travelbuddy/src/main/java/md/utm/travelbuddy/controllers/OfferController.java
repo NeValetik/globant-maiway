18b1cfad0de9a7ea3cbe4268c2c735ee43cf11c9
NeValetik <snowchug@gmail.com>
2024-09-12 09:46:13 +0300

    working o beauty

--- backend/travelbuddy/src/main/java/md/utm/travelbuddy/controllers/OfferController.java
+++ backend/travelbuddy/src/main/java/md/utm/travelbuddy/controllers/OfferController.java
@@ -3,7 +3,7 @@ package md.utm.travelbuddy.controllers;
 import md.utm.travelbuddy.models.Offer;
 import md.utm.travelbuddy.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
